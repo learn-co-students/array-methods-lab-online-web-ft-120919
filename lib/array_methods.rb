f432c3bf5554be7dcec5c9feec29d4949288c0af
Asha Fernandez <ash.fernan90@gmail.com>
2019-12-15 08:06:35 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,26 @@
-def using_include(array, element)
-
+def using_include["red", "pink", "green", "blue"]
+  using_include.include?("pink")
+   => false
+  using_includeinclude?("orange")
+   => true
 end
 
-def using_sort(array)
-
+def using_sort["red", "pink", "green", "blue"]
+   using_sort.sort
 end
 
-def using_reverse(array)
-
+def using_reverse["red", "pink", "green", "blue"]
+    using_reverse.reverse
 end
 
-def using_first(array)
-
+def using_first["red", "pink", "green", "blue"]
+    using_first.first
 end
 
-def using_last(array)
-
+def using_last["red", "pink", "green", "blue"]
+    using_last.last
 end
 
-def using_size(array)
-
+def using_size["red", "pink", "green", "blue"]
+    using_size.size
 end
